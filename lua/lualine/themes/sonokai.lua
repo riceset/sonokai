c97702eef6734aa23546f0f35d22c459bfac0cab
komeno <tkomeno@student.42tokyo.jp>
2022-04-17 03:26:57 -0300

    transparent lualine

--- lua/lualine/themes/sonokai.lua
+++ lua/lualine/themes/sonokai.lua
@@ -16,32 +16,32 @@ end
 return {
   normal = {
     a = {bg = palette.bg_blue[1], fg = palette.bg0[1], gui = 'bold'},
-    b = {bg = palette.bg4[1], fg = palette.fg[1]},
+    b = {bg = palette.bg1[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   insert = {
     a = {bg = palette.bg_green[1], fg = palette.bg0[1], gui = 'bold'},
-    b = {bg = palette.bg4[1], fg = palette.fg[1]},
+    b = {bg = palette.bg1[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   visual = {
     a = {bg = palette.bg_red[1], fg = palette.bg0[1], gui = 'bold'},
-    b = {bg = palette.bg4[1], fg = palette.fg[1]},
+    b = {bg = palette.bg1[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   replace = {
     a = {bg = palette.orange[1], fg = palette.bg0[1], gui = 'bold'},
-    b = {bg = palette.bg4[1], fg = palette.fg[1]},
+    b = {bg = palette.bg1[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   command = {
     a = {bg = palette.yellow[1], fg = palette.bg0[1], gui = 'bold'},
-    b = {bg = palette.bg4[1], fg = palette.fg[1]},
+    b = {bg = palette.bg1[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   terminal = {
     a = {bg = palette.purple[1], fg = palette.bg0[1], gui = 'bold'},
-    b = {bg = palette.bg3[1], fg = palette.fg[1]},
+    b = {bg = palette.bg1[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   inactive = {
